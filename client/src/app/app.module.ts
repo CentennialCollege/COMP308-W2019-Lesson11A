b30533b30882c3aac3364dd8b3b61473bff948d4
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2019-03-16 12:12:48 -0400

    added auth service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,14 +16,20 @@ import { ServicesComponent } from './pages/services/services.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-
-// services
-import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
 import { ContactDeleteComponent } from './contacts/contact-delete/contact-delete.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
+
+// services
+import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
+import { JwtModule, JwtHelperService, JwtInterceptor } from '@auth0/angular-jwt';
+import { AuthService } from './services/auth.service';
+
+export function jwtTokenGetter() {
+  return localStorage.getItem('id_token');
+}
 
 @NgModule({
   declarations: [
@@ -48,7 +54,12 @@ import { LoginComponent } from './pages/login/login.component';
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
-    FlashMessagesModule
+    FlashMessagesModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: jwtTokenGetter
+      }
+    })
   ],
   providers: [FlashMessagesService],
   bootstrap: [AppComponent]
