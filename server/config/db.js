37b8365bb08232d73480e15e93bf1f94bc539542
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2019-03-30 13:02:24 -0400

    modified for mongodb atlas and heroku

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = {
 //"URI" : "mongodb://localhost/test",
-"URI" : "mongodb://thomas:a123456@ds058739.mlab.com:58739/comp308-w2019-lesson10a",
+"URI": "mongodb+srv://thomas:a123456@cluster0-hrrta.azure.mongodb.net/test?retryWrites=true",
+//"URI" : "mongodb://thomas:a123456@ds058739.mlab.com:58739/comp308-w2019-lesson10a",
 "secret": "SomeSecret"
 }
